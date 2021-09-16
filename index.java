2708449130d895a2c08c19403ab761b0495c6959
Ridwanulllah1 <82841714+Ridwanulllah1@users.noreply.github.com>
2021-09-16 17:13:35 +0100

    Add files via upload
    Here are  the answers

---
+++ index.java
@@ -0,0 +1,13 @@
+class GravityCalc{
+      Public Static Void main(string[] args){
+        double gravity= -9.81;
+        double initialVelocity= 0.0;
+        double fallingTime= 10.0;
+        double initialPosition= 0.0;
+        double finalPosition= 0.0;
+        finalPosition= 0.5 *gravity*(fallingTime*FallingTime)+ initialVelocity + initialPosition;
+        System.Out.PrintIn("The object's Position After" + fallingTime+ "seconds is" + finalPosition+"m");
+     }
+  }
+        
+}
